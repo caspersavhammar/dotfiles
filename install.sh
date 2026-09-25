22b5c1a7ef2adbe3b31dd02758f152ddbf8ad1a8
casav <casper@savhammar.se>
2026-09-25 22:51:12 +0200

    install.sh | added ~/ everywhere

--- install.sh
+++ install.sh
@@ -4,25 +4,23 @@ cd ~
 #git clone git@github.com:caspersavhammar/dotfiles.git
 #mv dotfiles .dotfiles
 
-rm -rf .config/sway .config/waybar
+rm -rf ~/.config/sway .config/waybar
 ln -s ~/.dotfiles/swayCachy ~/.config/sway
 ln -s ~/.dotfiles/waybarSway ~/.config/waybar
-mkdir -p Pictures/background
+mkdir -p ~/Pictures/background
 ln -s ~/.dotfiles/Artemis.jpeg ~/Pictures/background
-rm .zshrc
+rm ~/.zshrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 sudo pacman -Syu
 sudo pacman -S dolphin wl-mirror brighnessctl npm lazygit swaync swaybg swaylock ttf-jetbrains-mono-nerd valgrind gdb gcc python zsh-autosuggestions zsh-syntax-highlighting zsh lua luarocks okular fastfetch fzf neovim vim vi btop htop bat scrot tmux rofi typst ghostty discord \ 
 
-cd ~/.config
-
-git clone git@github.com:caspersavhammar/ghosttyConfig.git
-mv ghosttyConfig ghostty
-git clone git@github.com:caspersavhammar/tmuxConfig.git
-mv tmuxConfig tmux
-git clone git@github.com:caspersavhammar/nvimConfig.git
-mv nvimConfig nvim
+git clone git@github.com:caspersavhammar/ghosttyConfig.git ~/.config/
+mv ~/.config/ghosttyConfig ~/.config/ghostty
+git clone git@github.com:caspersavhammar/tmuxConfig.git ~/.config/
+mv ~/.config/tmuxConfig ~/.config/tmux
+git clone git@github.com:caspersavhammar/nvimConfig.git ~/.config/
+mv ~/.config/nvimConfig ~/.config/nvim
 
 cd ~
 git clone https://aur.archlinux.org/paru.git
